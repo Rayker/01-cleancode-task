6d24324dc6ed3f6ec96e28208c6f06aa2186c87a
Tatarinov Denis <denis160995@yandex.ru>
2014-11-28 12:37:35 +0400

    Decomposition

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -5,26 +5,21 @@ namespace CleanCode
 {
 	public static class RefactorMethod
 	{
-		private static void SaveData(string s, byte[] d)
+	    private static void WriteDataToFile(string path, byte[] data)
+	    {
+            var fs = new FileStream(path, FileMode.OpenOrCreate);
+	        fs.Write(data, 0, data.Length);
+            fs.Close();
+	    }
+		private static void SaveData(string path, byte[] data)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
-
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
-
-			// close files
-			fs1.Close();
-			fs2.Close();
+            WriteDataToFile(path, data);
+            WriteDataToFile(Path.ChangeExtension(path, "bkp"), data);
 
 			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
+			string timeFile = path + ".time";
 			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+            WriteDataToFile(timeFile, t);
 		}
 	}
 }
